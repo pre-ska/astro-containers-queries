56516397dd5424090f0bdc25752a5890507e91b8
preska <wolfwar@outlook.com>
2023-09-22 13:20:46 +0200

    img fix

--- src/products.ts
+++ src/products.ts
@@ -2,31 +2,31 @@ export default [
   {
     id: "woman-blouse-1",
     name: "Woman's Pullover",
-    src: "/assets/woman-blouse-1.jpeg",
+    src: "assets/woman-blouse-1.jpeg",
   },
   {
     id: "woman-blouse-2",
     name: "Woman's Pullover",
-    src: "/assets/woman-blouse-2.jpeg",
+    src: "assets/woman-blouse-2.jpeg",
   },
   {
     id: "woman-pullover-1",
     name: "Woman's Blouse",
-    src: "/assets/woman-pullover-1.jpg",
+    src: "assets/woman-pullover-1.jpg",
   },
   {
     id: "woman-pullover-2",
     name: "Woman's Blouse",
-    src: "/assets/woman-pullover-2.jpg",
+    src: "assets/woman-pullover-2.jpg",
   },
   {
     id: "man-shirt-1",
     name: "Man's Shirt",
-    src: "/assets/man-shirt-1.jpg",
+    src: "assets/man-shirt-1.jpg",
   },
   // {
   //   id: "man-shirt-2",
   //   name: "Man's Shirt",
-  //   src: "/assets/man-shirt-2.jpg",
+  //   src: "assets/man-shirt-2.jpg",
   // },
 ]
