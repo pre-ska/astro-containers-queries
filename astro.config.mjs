a6273cf10d7f85b19e3831757758c5de871cd19d
preska <wolfwar@outlook.com>
2023-09-22 11:34:50 +0200

    config fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from "astro/config"
 
 import tailwind from "@astrojs/tailwind"
 
-import { defineConfig } from "astro/config"
-
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://pre-ska.github.io",
