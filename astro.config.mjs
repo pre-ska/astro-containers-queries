e7bab3aebcb15ab98f0f62e2fc34c84d9215b260
preska <wolfwar@outlook.com>
2023-09-22 13:31:22 +0200

    revert to previos

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,4 @@ import tailwind from "@astrojs/tailwind"
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://pre-ska.github.io",
-  base: "/astro-containers-queries",
 })
