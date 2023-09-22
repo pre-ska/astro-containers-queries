55cfa2a28f59b8929faaeee1d76f60670392d270
preska <wolfwar@outlook.com>
2023-09-22 11:15:26 +0200

    left nav

--- src/products.ts
+++ src/products.ts
@@ -1,22 +1,22 @@
 export default [
   {
     id: "woman-blouse-1",
-    name: "Woman's Blouse",
+    name: "Woman's Pullover",
     src: "/assets/woman-blouse-1.jpeg",
   },
   {
     id: "woman-blouse-2",
-    name: "Woman's Blouse",
+    name: "Woman's Pullover",
     src: "/assets/woman-blouse-2.jpeg",
   },
   {
     id: "woman-pullover-1",
-    name: "Woman's Pullover",
+    name: "Woman's Blouse",
     src: "/assets/woman-pullover-1.jpg",
   },
   {
     id: "woman-pullover-2",
-    name: "Woman's Pullover",
+    name: "Woman's Blouse",
     src: "/assets/woman-pullover-2.jpg",
   },
   {
